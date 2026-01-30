e0fc2cd08364e82cd6221fafa89c7ba0b4923f14
Pierre Loiez <pierre.loiez@student.isae-supaero.fr>
2026-01-30 22:20:47 +0100

    Added solution to Euler 8

---
+++ solutions/euler_8.py
@@ -0,0 +1,15 @@
+    
+def Euler8():
+    with open('./resources/euler_8_num.txt') as file:
+        fullInt = ''.join(file.readline()[:-1] for _ in range(20))
+    best = 0
+    for i in range(len(fullInt)-13):
+        num = fullInt[i:i+13]
+        res = 1
+        for dig in num:
+            res = res*int(dig)
+        if res>best:
+            best=res
+    return best
+
+print(Euler8())
