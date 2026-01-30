465ea96b806d908238cf03248e39ba0cb6c71b48
Pierre Loiez <pierre.loiez@student.isae-supaero.fr>
2026-01-30 22:20:19 +0100

    Added solution to Euler 4

---
+++ solutions/euler_4.py
@@ -0,0 +1,15 @@
+def checkPal(number):
+    strNum = str(number)
+    return strNum == strNum[::-1]
+
+def Euler4():
+    best=0
+    for i in range(999, 100, -1):
+        for j in range(i, 100, -1):
+            if checkPal(i*j) and i*j>best:
+                best = i*j
+            if i*i<best:
+                return best
+    return best
+
+print(Euler4())
